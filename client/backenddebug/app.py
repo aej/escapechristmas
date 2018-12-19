58bc6550dccef49a0870486518aca05e76c0bc7d
Alex Roe <alex.roe@growthstreet.co.uk>
2018-12-19 20:36:23 +0000

    Send infinite tweets

--- client/backenddebug/app.py
+++ client/backenddebug/app.py
@@ -1,24 +1,36 @@
-from time import sleep
+import eventlet
+eventlet.monkey_patch()
+
 from random import randint
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 from flask_socketio import send, emit
 
+#!/usr/bin/env python
+
+import eventlet
+eventlet.monkey_patch()
+
+from flask import Flask
+from flask_socketio import SocketIO
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socket = SocketIO(app, logger=True, engineio_logger=True)
 
+def send_tweet():
+    data = {
+        'lat': randint(-180, 180),
+        'long': randint(-180, 180),
+    }
+    socket.emit('tweet', data)
 
-@socketio.on('connect')
-def mysocket():
+def send_infinite_tweets():
     while True:
-        data = {
-            'lat': randint(-180, 180),
-            'long': randint(-180, 180),
-        }
-        emit('tweet', data)
-        sleep(1)
+        send_tweet()
+        eventlet.sleep(1)
 
+eventlet.spawn(send_infinite_tweets)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socket.run(app)
+
